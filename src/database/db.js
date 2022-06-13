11f7f3daca05003d45139de68a8e9415acbf3683
manopassaro <103702640+manopassaro@users.noreply.github.com>
2022-06-13 19:26:30 -0300

    db

--- src/database/db.js
+++ src/database/db.js
@@ -2,8 +2,12 @@ require('dotenv').config();
 const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize(
-process.env.DATABASE_URL,
+    process.env.DB_BASE,
+    process.env.DB_USER,
+    process.env.DB_PASS,
     {
+        host: process.env.DB_HOST,
+        port: 5432,
         dialect: "postgres",
     }
 );
