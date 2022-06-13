db6f4580ac17487f361cdc2cb4b5ebdb48d77503
manopassaro <103702640+manopassaro@users.noreply.github.com>
2022-06-13 19:24:12 -0300

    db

--- src/database/db.js
+++ src/database/db.js
@@ -1,15 +1,10 @@
 require('dotenv').config();
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DATABASE_URL,
+const sequelize = new Sequelize(
+process.env.DATABASE_URL,
     {
         dialect: "postgres",
-        dialectOptions: {
-            ssl: {
-                require: true,
-                rejectUnauthorized: false
-            },
-        },
     }
 );
 
