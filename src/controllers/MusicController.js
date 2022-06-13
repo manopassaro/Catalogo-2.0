35e6714c89ec69676972277079f8ef936cccd77a
manopassaro <103702640+manopassaro@users.noreply.github.com>
2022-06-13 18:43:17 -0300

    order and details

--- src/controllers/MusicController.js
+++ src/controllers/MusicController.js
@@ -2,7 +2,7 @@ const Song = require('../models/Music');
 
 const getAll = async (req, res) => {
     try{
-        const songs = await Song.findAll();
+        const songs = await Song.findAll({order: [["id", "ASC"]]});
         res.render("inicial", {songs})
     }catch(err) {
         res.status(500).send({err: err.message})
